f8e700965ce2e6c7e65735a3a4f04d7c6417f0a7
Katie Marie <33885541+kguite@users.noreply.github.com>
2021-06-04 14:10:33 -0700

    updated name

--- _data/posts.js
+++ _data/posts.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 module.exports = async () => {
-    const result = await axios.get('https://collectednotes.com/jenaro.json')
+    const result = await axios.get('https://collectednotes.com/katiemarie.json')
     return {
         posts: result.data.notes
     }
